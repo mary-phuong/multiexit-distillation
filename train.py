041fddf2e56e0f0b6d0bcb70c904edb1205e9936
Mary Phuong <mariana.phuong@gmail.com>
2019-10-24 15:33:43 +0200

    added comments train_cifar.py

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import sacred
 from sacred.observers import FileStorageObserver
-import time
 import torch
 import main, utils
 from utils import device, dict_drop, RUNS_DB_DIR
@@ -31,9 +30,9 @@ def get_net(cf_net, parent_snapshot, gpu):
 #########################################################
 
 @ex.main
-def train(cf_init, cf_trn, cf_val, cf_opt, cf_loss, cf_grad_transforms,
-          cf_scheduler, batch_size, val_batch_size, gpu, time_budget, n_epochs,
-          parent_snapshot, snapshot_name, log_trn_full, save_interval):
+def train(cf_trn, cf_val, cf_opt, cf_loss,
+          cf_scheduler, batch_size, val_batch_size, gpu, n_epochs,
+          parent_snapshot, snapshot_name, save_interval):
 
     torch.backends.cudnn.benchmark = True
     main_gpu = gpu if isinstance(gpu, int) else gpu[0]
@@ -41,10 +40,6 @@ def train(cf_init, cf_trn, cf_val, cf_opt, cf_loss, cf_grad_transforms,
     
     net = get_net()
     loss_f = getattr(main, cf_loss['call'])(**dict_drop(cf_loss, 'call'))
-
-    init = getattr(main, cf_init['call'])(**dict_drop(cf_init, 'call'))
-    for p in net.parameters():
-        init(p)
     
     Opt = getattr(torch.optim, cf_opt['call'])
     opt = Opt(net.parameters(), **dict_drop(cf_opt, 'call'))
@@ -61,33 +56,24 @@ def train(cf_init, cf_trn, cf_val, cf_opt, cf_loss, cf_grad_transforms,
     Data = getattr(main, cf_val['call'])
     val_iter = Data(split='val', batch_size=val_batch_size, gpu=main_gpu,
                     **dict_drop(cf_val, 'call'))
-
-    grad_tforms = []
-    for cf in cf_grad_transforms:
-        grad_tforms += [getattr(main, cf['call'])(**dict_drop(cf, 'call'))]
     
     if save_interval:
         saver = utils.IntervalSaver(snapshot_name, ep, save_interval)
     else:
         saver = utils.RecordSaver(snapshot_name, ep)
-    start_time = time.time()
     print('\t'.join(['ep', 'loss'] + loss_f.metric_names))
     
-    while time.time() - start_time < 3600 * time_budget or ep < n_epochs:
+    while ep < n_epochs:
         scheduler.step()
         for trn_tuple in trn_iter:
             trn_tuple = [t.to(dvc) for t in trn_tuple]
             opt.zero_grad()
             loss = loss_f(net.train(True), *trn_tuple, ep)
             loss.backward()
-            for tform in grad_tforms:
-                for par in net.parameters():
-                    tform(par.grad.data)
             opt.step()
             
         ep += 1
-        trn_metrics = (main.validate(loss_f, net, trn_iter, main_gpu)
-                       if log_trn_full else loss_f.trn_metrics())
+        trn_metrics = loss_f.trn_metrics()
         val_metrics = main.validate(loss_f, net, val_iter, main_gpu)
         print(utils.tab_str(ep, loss, *trn_metrics))
         print(utils.tab_str('', 0.0, *val_metrics))
--- train_cifar.py
+++ train_cifar.py
@@ -4,13 +4,13 @@ import utils
 
 @ex.config
 def config_new():
-    gpu = 0               # single GPU ordinal or list, or -1 for CPU
+    gpu = 0                 # single GPU ordinal or list, or -1 for CPU
     snapshot_name = 'test:0'
-    parent_snapshot = ''  # If empty, trains from scratch. Otherwise loads the
-                          # specified snapshot and continues training.
+    parent_snapshot = ''    # If empty, trains from scratch. Otherwise loads the
+                            # specified snapshot and continues training.
 
                           
-    cf_net = dict(        # MSDNet architecture parameters
+    cf_net = dict(          # MSDNet architecture parameters
         call = 'MsdNet',
         in_shape = 32,
         out_dim = 100,
@@ -26,72 +26,70 @@ def config_new():
         exit_width = 128,
         btneck_widths = [4, 4, 4],
     )
+
     
-    cf_loss = dict(       # train with classification loss only
-        call = 'ClassificationOnlyLoss',
+    cf_loss = dict(         # distillation-based training with temperature
+                            # annealing
+        call = 'DistillationBasedLoss',
         n_exits = cf_net['n_exits'],
         acc_tops = [1, 5],
+        
+        C = 0.5,
+        maxprob = 0.5,
+        global_scale = 2.0 * 5/cf_net['n_exits'],
     )
 
-    # cf_loss = dict(       # distillation-based training  
-    #     call = 'DistillationBasedLoss',
+    # cf_loss = dict(       # distillation-based training with constant
+    #                       # temperature
+    #     call = 'DistillationLossConstTemp',
     #     n_exits = cf_net['n_exits'],
     #     acc_tops = [1, 5],
         
     #     C = 0.5,
-    #     maxprob = 0.5,
+    #     T = 4.0,
     #     global_scale = 2.0 * 5/cf_net['n_exits'],
     # )
 
-    # cf_loss = dict(
-    #     call = 'MultiFrzDistLossConstTemp',
-    #     C = 0.5,
-    #     T = 4.0,
-    #     weight_last = True,
-    #     global_scale = 2.0 * 5/cf_net['n_exits'],
-    #     freeze = False,
+    # cf_loss = dict(       # train with classification loss only
+    #     call = 'ClassificationOnlyLoss',
+    #     n_exits = cf_net['n_exits'],
+    #     acc_tops = [1, 5],
     # )
 
     
-    cf_trn = dict(
+    cf_trn = dict(          # training set parameters
         call = 'Cifar100',
-        n_per_class = 80,
+        n_per_class = 150,  # number of images per class (including validation)
         nval_per_class = 50,
-        # n_unl_per_class = 400,
-        # lab_unl_ratio = (1, 1),
-        augment = True,
+        augment = True,     # data augmentation
         seed = 0,
     )
     cf_val = cf_trn.copy()
     cf_val['augment'] = False
+
     
-    cf_opt = dict(
+    cf_opt = dict(          # optimization method
         call = 'SGD',
         lr = 0.1,
         momentum = 0.9,
         weight_decay = 1e-4,
         nesterov = True,
     )
-    batch_size = 64 #!
-    val_batch_size = 320
-    time_budget = 0  # in hours
-    n_epochs = 1
-    cf_scheduler = dict(
+    
+    batch_size = 64
+    val_batch_size = 250
+    n_epochs = 300
+    
+    cf_scheduler = dict(   # learning rate schedule
         call = 'MultiStepLR',
         milestones = [150, 225],
         gamma = 0.1
     )
     
-    log_trn_full = False
-    save_interval = 0
+    save_interval = 0      # if nonzero, save a snapshot every X epochs,
+                           # otherwise save a snapshot when it's the best so far
 
-    #### rarely used params #######################
-    cf_grad_transforms = []
-    cf_init = dict(
-        call = 'ScaleParamInit',
-        scale = 1.0,
-    )
-
+                           
 
 if __name__ == '__main__':
     ex.run_commandline()
